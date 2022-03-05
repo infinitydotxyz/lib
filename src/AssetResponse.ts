559c1bab7b1e850655b9d5efb7e0458267ba58f2
Adi Kancherla <findadit@gmail.com>
2022-03-05 12:18:54 -0500

    types

--- src/AssetResponse.ts
+++ src/AssetResponse.ts
@@ -1,6 +1,6 @@
 import { AlchemyUserAssetResponse, AlchemyUserAsset } from './services/alchemy/AlchemyUserAsset';
 import { CovalentWalletBalanceItem } from './services/covalent/CovalentNFTMetadata';
-import { UnmarshalNFTAsset, UnmarshalUserAssetResponse } from './services/unmarshal/UnmarshalUserAsset';
+import { UnmarshalNFTAsset, UnmarshalUserAssetResponse } from './services/unmarshal/Unmarshal';
 import { WyvernAssetData } from './wyvern/WyvernOrder';
 
 export interface AssetResponse extends AlchemyUserAssetResponse, UnmarshalUserAssetResponse {
