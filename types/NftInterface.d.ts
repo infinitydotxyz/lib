f149d2268dbe84883d27f090a0fdd1814c680c74
Adi Kancherla <findadit@gmail.com>
2022-03-05 12:11:45 -0500

    fe types

--- types/NftInterface.d.ts
+++ types/NftInterface.d.ts
@@ -156,3 +156,128 @@ export interface Links {
     wiki?: string;
     facebook?: string;
 }
+export interface Order {
+    id: string;
+    blueCheck: boolean;
+    howToCall: number;
+    salt: string;
+    feeRecipient: string;
+    staticExtradata: string;
+    quantity: string;
+    staticTarget: string;
+    maker: string;
+    side: number;
+    takerProtocolFee: string;
+    saleKind: number;
+    basePrice: number;
+    metadata: Metadata;
+    extra: string;
+    expirationTime: string;
+    hasBonusReward: boolean;
+    calldata: string;
+    hash: string;
+    r: string;
+    replacementPattern: string;
+    taker: string;
+    takerRelayerFee: string;
+    s: string;
+    makerRelayerFee: string;
+    listingTime: string;
+    target: string;
+    v: number;
+    makerProtocolFee: string;
+    paymentToken: string;
+    feeMethod: number;
+    exchange: string;
+    makerReferrerFee: string;
+}
+export declare enum OrderType {
+    BUY = 0,
+    SELL = 1
+}
+export interface TraitItem {
+    traitType: string;
+    traitValue: string;
+}
+export interface Metadata {
+    asset: Asset;
+    hasBonusReward: boolean;
+    schema: string;
+    hasBlueCheck: boolean;
+    createdAt: number;
+    basePriceInEth: number;
+    listingType: string;
+    chainId: string;
+}
+export interface Asset {
+    id: string;
+    address: string;
+    quantity: string;
+    imagePreview: string;
+    title: string;
+    description: string;
+    image: string;
+    owner: string;
+    collectionName: string;
+    searchCollectionName?: string;
+    searchTitle?: string;
+    traits?: TraitItem[];
+}
+export interface Orders {
+    count: number;
+    listings: Order[];
+}
+export declare type BaseCardData = {
+    id: string;
+    title: string;
+    name?: string;
+    description?: string;
+    image?: string;
+    cardImage?: string;
+    imagePreview?: string;
+    price?: number;
+    inStock?: number;
+    order?: Order;
+    tokenAddress?: string;
+    tokenId?: string;
+    collectionName?: string;
+    maker?: string;
+    hasBonusReward?: boolean;
+    hasBlueCheck?: boolean;
+    owner?: string;
+    metadata?: Metadata;
+    schemaName?: string;
+    expirationTime?: string;
+    chainId?: string;
+};
+export declare type CardData = BaseCardData & {
+    openseaListing?: BaseCardData;
+};
+export declare enum WyvernSchemaName {
+    ERC20 = "ERC20",
+    ERC721 = "ERC721",
+    ERC1155 = "ERC1155",
+    LegacyEnjin = "Enjin",
+    ENSShortNameAuction = "ENSShortNameAuction",
+    CryptoPunks = "CryptoPunks"
+}
+/**
+ * The NFT version that this contract uses.
+ * ERC721 versions are:
+ * 1.0: CryptoKitties and early 721s, which lack approve-all and
+ *      have problems calling `transferFrom` from the owner's account.
+ * 2.0: CryptoSaga and others that lack `transferFrom` and have
+ *      `takeOwnership` instead
+ * 3.0: The current OpenZeppelin standard:
+ *      https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/token/ERC721/ERC721.sol
+ * Special cases:
+ * locked: When the transfer function has been locked by the dev
+ */
+export declare enum TokenStandardVersion {
+    Unsupported = "unsupported",
+    Locked = "locked",
+    Enjin = "1155-1.0",
+    ERC721v1 = "1.0",
+    ERC721v2 = "2.0",
+    ERC721v3 = "3.0"
+}
--- types/NftInterface.js
+++ types/NftInterface.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ListingType = exports.OrderSide = void 0;
+exports.TokenStandardVersion = exports.WyvernSchemaName = exports.OrderType = exports.ListingType = exports.OrderSide = void 0;
 var OrderSide;
 (function (OrderSide) {
     OrderSide[OrderSide["Buy"] = 0] = "Buy";
@@ -12,3 +12,38 @@ var ListingType;
     ListingType["DutchAuction"] = "dutchAuction";
     ListingType["EnglishAuction"] = "englishAuction";
 })(ListingType = exports.ListingType || (exports.ListingType = {}));
+var OrderType;
+(function (OrderType) {
+    OrderType[OrderType["BUY"] = 0] = "BUY";
+    OrderType[OrderType["SELL"] = 1] = "SELL";
+})(OrderType = exports.OrderType || (exports.OrderType = {}));
+var WyvernSchemaName;
+(function (WyvernSchemaName) {
+    WyvernSchemaName["ERC20"] = "ERC20";
+    WyvernSchemaName["ERC721"] = "ERC721";
+    WyvernSchemaName["ERC1155"] = "ERC1155";
+    WyvernSchemaName["LegacyEnjin"] = "Enjin";
+    WyvernSchemaName["ENSShortNameAuction"] = "ENSShortNameAuction";
+    WyvernSchemaName["CryptoPunks"] = "CryptoPunks";
+})(WyvernSchemaName = exports.WyvernSchemaName || (exports.WyvernSchemaName = {}));
+/**
+ * The NFT version that this contract uses.
+ * ERC721 versions are:
+ * 1.0: CryptoKitties and early 721s, which lack approve-all and
+ *      have problems calling `transferFrom` from the owner's account.
+ * 2.0: CryptoSaga and others that lack `transferFrom` and have
+ *      `takeOwnership` instead
+ * 3.0: The current OpenZeppelin standard:
+ *      https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/contracts/token/ERC721/ERC721.sol
+ * Special cases:
+ * locked: When the transfer function has been locked by the dev
+ */
+var TokenStandardVersion;
+(function (TokenStandardVersion) {
+    TokenStandardVersion["Unsupported"] = "unsupported";
+    TokenStandardVersion["Locked"] = "locked";
+    TokenStandardVersion["Enjin"] = "1155-1.0";
+    TokenStandardVersion["ERC721v1"] = "1.0";
+    TokenStandardVersion["ERC721v2"] = "2.0";
+    TokenStandardVersion["ERC721v3"] = "3.0";
+})(TokenStandardVersion = exports.TokenStandardVersion || (exports.TokenStandardVersion = {}));
