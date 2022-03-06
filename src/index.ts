7f9705b908a93f897e1b6c121778b9b39586e6b1
Sleeyax <yourd3veloper@gmail.com>
2022-03-06 01:50:23 +0100

    Revert "include empty index file"
    This reverts commit 2514ec2f66fe3bb351d92c18abd18378c00a59fe.

--- src/index.ts
+++
@@ -1,2 +0,0 @@
-// This file is empty because we want to enforce users to import from the subfolders in this directory (src).
-// Adding aditional imports here could lead to inconsistent imports when the package is consumed.
