16d86dbd12ba4ca9cbd98c36cf41c1d839327389
Adi Kancherla <findadit@gmail.com>
2022-03-05 23:56:21 -0500

    publish

--- src/core/OpenSeaAsset.ts
+++ src/core/OpenSeaAsset.ts
@@ -1,20 +1,6 @@
 import { WyvernAssetData } from '../protocols/wyvern/WyvernOrder';
 import { Asset } from './Asset';
 import { Trait } from './Trait';
-
 export interface OpenSeaAsset extends Asset {
-  title: string;
-  traits: Trait[];
-  searchTitle: string;
-  traitValues: (string | number)[];
-  numTraits: number;
-  rawData: WyvernAssetData;
-  collectionName: string;
-  id: string;
-  description: string;
-  image: string;
-  searchCollectionName: string;
-  address: string;
-  imagePreview: string;
-  traitTypes: string[];
+  rawData?: WyvernAssetData;
 }
