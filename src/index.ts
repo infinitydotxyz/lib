2514ec2f66fe3bb351d92c18abd18378c00a59fe
Sleeyax <yourd3veloper@gmail.com>
2022-03-06 01:32:13 +0100

    include empty index file

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+// This file is empty because we want to enforce users to import from the subfolders in this directory (src).
+// Adding aditional imports here could lead to inconsistent imports when the package is consumed.
