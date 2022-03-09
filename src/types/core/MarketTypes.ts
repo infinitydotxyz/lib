17ed6879bfbd8b953d87bf67edea171c0fa3e0fd
Steve Gehrman <sgehrman@gmail.com>
2022-03-09 03:12:37 -0800

    fix market types

--- src/types/core/MarketTypes.ts
+++ src/types/core/MarketTypes.ts
@@ -1,16 +1,15 @@
 export interface MarketOrder {
+  user: string;
   expiration: number;
 }
 
 export interface BuyOrder extends MarketOrder {
-  user: string;
   collections: string[];
   minNFTs: number;
   budget: number;
 }
 
 export interface SellOrder extends MarketOrder {
-  user: string;
   nftAddress: string;
   collection: string;
   price: number;
