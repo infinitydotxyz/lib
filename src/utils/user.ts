7aaa7359693a0c53de91f4fb0ee30b9abd6d4745
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-03-16 11:29:19 -0500

    added helper function to get user display name

---
+++ src/utils/user.ts
@@ -0,0 +1,18 @@
+import { providers } from 'ethers';
+
+export async function getUserDisplayName(address: string, chainId: string, provider: providers.JsonRpcProvider) {
+    try {
+        // TODO support users setting their own display name
+
+        // ethers.js automatically checks that the forward resolution matches the reverse lookup
+        const ensName = await provider.lookupAddress(address);
+
+        if (ensName) {
+            return ensName;
+        }
+    } catch (err) {
+
+    } finally {
+        return address;
+    }
+}
