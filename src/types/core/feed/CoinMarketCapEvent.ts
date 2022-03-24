c786652bbd8e7e3ab04b9a9d0d0f091aecc65568
Sleeyax <yourd3veloper@gmail.com>
2022-03-24 23:57:41 +0100

    simplify cmc model
    we shouldn't store data we don't need

--- src/types/core/feed/CoinMarketCapEvent.ts
+++ src/types/core/feed/CoinMarketCapEvent.ts
@@ -14,68 +14,51 @@ export interface CoinMarketCapNewsEvent extends BaseFeedEvent {
   /**
    * Date the news item was added to CMC.
    */
-  createdAt: string;
+  createdAtCMC: string;
 
-  /**
-   * Meta data.
-   */
-  meta: CoinMarketCapMeta;
-}
-
-export interface CoinMarketCapMeta {
   /**
    * Title of the article.
    */
-  title: string;
+   title: string;
 
-  /**
-   * Subtitle of the article.
-   */
-  subtitle: string;
+   /**
+    * Subtitle of the article.
+    */
+   subtitle: string;
 
-  /**
-   * Article contents.
-   * Ususally this doesn't contain the while article body; users should visit the source to read more.
-   */
-  content: string;
+   /**
+    * Article contents.
+    * Ususally this doesn't contain the while article body; users should visit the source to read more.
+    */
+   content: string;
 
-  /**
-   * Name of the source where the article originally came from.
-   */
-  sourceName: string;
+   /**
+    * Name of the source where the article originally came from.
+    */
+   sourceName: string;
 
-  /**
-   * Original source URL of the article.
-   */
-   sourceUrl: string;
+   /**
+    * Original source URL of the article.
+    */
+    sourceUrl: string;
 
-  /**
-   * Language the article was written in.
-   */
-  language: string;
+   /**
+    * Language the article was written in.
+    */
+   language: string;
 
-  /**
-   * Publish status.
-   */
-  status: string;
+   /**
+    * Date the article was published at the specific source.
+    */
+   createdAt: string;
 
-  /**
-   * Whether the article is visible to the user.
-   */
-  visibility: boolean;
+   /**
+    * Date the article was updated at the specific source.
+    */
+   updatedAt: string;
 
-  /**
-   * Date the article was published at the specific source.
-   */
-  createdAt: string;
-
-  /**
-   * Date the article was updated at the specific source.
-   */
-  updatedAt: string;
-
-  /**
-   * Date the article was publically released at the specific source.
-   */
-  releasedAt: string;
+   /**
+    * Date the article was publically released at the specific source.
+    */
+   releasedAt: string;
 }
