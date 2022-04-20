209e313f4922d2f7f1c8aac1906fbeb60ec849bc
Adi Kancherla <findadit@gmail.com>
2022-04-20 19:05:25 +0530

    orders

--- src/utils/orders.ts
+++ src/utils/orders.ts
@@ -1,6 +1,6 @@
 import { keccak256 } from '@ethersproject/solidity';
 import { error } from 'console';
-import { ETHEREUM_WETH_ADDRESS, NULL_ADDRESS, POLYGON_INFINITY_OB_COMPLICATION_ADDRESS, POLYGON_WETH_ADDRESS } from './constants';
+import { ETHEREUM_WETH_ADDRESS, NULL_ADDRESS, POLYGON_INFINITY_EXCHANGE_ADDRESS, POLYGON_INFINITY_FEE_TREASURY_ADDRESS, POLYGON_INFINITY_OB_COMPLICATION_ADDRESS, POLYGON_WETH_ADDRESS } from './constants';
 
 export function getOrderId(user: string, nonce: string, chainId: string) {
   try {
@@ -10,7 +10,7 @@ export function getOrderId(user: string, nonce: string, chainId: string) {
   }
 }
 
-export function getCurrencyAddress(chainId: string) {
+export function getTxnCurrencyAddress(chainId: string) {
   if (chainId === '1') {
     return ETHEREUM_WETH_ADDRESS;
   } else if (chainId === '137') {
@@ -20,11 +20,31 @@ export function getCurrencyAddress(chainId: string) {
   }
 }
 
-export function getComplicationAddress(chainId: string) {
+export function getOBComplicationAddress(chainId: string) {
   if (chainId === '1') {
     return NULL_ADDRESS; // todo: change this
   } else if (chainId === '137') {
     return POLYGON_INFINITY_OB_COMPLICATION_ADDRESS;
+  } else {
+    return NULL_ADDRESS;
+  }
+}
+
+export function getExchangeAddress(chainId: string) {
+  if (chainId === '1') {
+    return NULL_ADDRESS; // todo: change this
+  } else if (chainId === '137') {
+    return POLYGON_INFINITY_EXCHANGE_ADDRESS;
+  } else {
+    return NULL_ADDRESS;
+  }
+}
+
+export function getFeeTreasuryAddress(chainId: string) {
+  if (chainId === '1') {
+    return NULL_ADDRESS; // todo: change this
+  } else if (chainId === '137') {
+    return POLYGON_INFINITY_FEE_TREASURY_ADDRESS;
   } else {
     return NULL_ADDRESS;
   }
