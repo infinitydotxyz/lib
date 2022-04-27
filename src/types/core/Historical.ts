a33c45cbcd5234bcbcfd99ed136f82f726c294f7
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-04-26 22:18:26 -0500

    minor

--- src/types/core/Historical.ts
+++ src/types/core/Historical.ts
@@ -1,11 +1,8 @@
-import { StringNumber } from './UtilityTypes';
 
-export type Hourly<Data> = Record<StringNumber, Data>;
+
 
 export interface WithTimestamp {
   timestamp: number;
 }
 
-export type HistoricalWeek<Data extends WithTimestamp, Aggregated extends WithTimestamp> = Hourly<Data> & {
-  aggregated: Aggregated;
-};
+export type HistoricalWeek<Data extends WithTimestamp, Aggregated extends WithTimestamp> = any
