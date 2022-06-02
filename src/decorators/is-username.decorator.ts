32e570202cecf4e3abeb322dec9b2969bedb5434
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-06-02 10:29:28 -0500

    removed circular dependency

--- src/decorators/is-username.decorator.ts
+++ src/decorators/is-username.decorator.ts
@@ -1,9 +1,17 @@
 import { registerDecorator, ValidationOptions } from 'class-validator';
-import { MAX_USERNAME_CHARS, MIN_USERNAME_CHARS, usernameChars } from '../types/dto/user';
+
+export const MIN_USERNAME_CHARS = 3;
+export const MAX_USERNAME_CHARS = 14;
+export const usernameChars = '[a-zA-Z0-9_]';
+export const usernameCharRegex = new RegExp(`[^${usernameChars}]`, 'g');
+const supportedChars = 'alphanumeric characters and underscores';
+export const usernameConstraints = `Username must be between ${MIN_USERNAME_CHARS} and ${MAX_USERNAME_CHARS} characters long and can contain only ${supportedChars}`;
+export const MAX_DISPLAY_NAME_CHARS = 50;
+export const MAX_BIO_CHARS = 160;
 
 export const usernameRegex = new RegExp(`^${usernameChars}{${MIN_USERNAME_CHARS},${MAX_USERNAME_CHARS}}$`);
 
-function isValidUsername(value: string) {
+export function isValidUsername(value: string) {
   if (typeof value !== 'string') {
     return false;
   }
--- src/types/dto/user/user-profile.dto.ts
+++ src/types/dto/user/user-profile.dto.ts
@@ -1,19 +1,8 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Transform } from 'class-transformer';
 import { IsEthereumAddress, IsNumber, IsString, MaxLength } from 'class-validator';
-import { IsUsername } from '../../../decorators';
+import { IsUsername, MAX_BIO_CHARS, MAX_DISPLAY_NAME_CHARS, usernameConstraints } from '../../../decorators';
 import { normalizeAddressTransformer } from '../../../transformers/normalize-address.transformer';
-
-export const MIN_USERNAME_CHARS = 3;
-export const MAX_USERNAME_CHARS = 14;
-export const usernameChars = '[a-zA-Z0-9_]';
-export const usernameCharRegex = new RegExp(`[^${usernameChars}]`, 'g');
-export const usernameRegex = new RegExp(`^${usernameChars}{${MIN_USERNAME_CHARS},${MAX_USERNAME_CHARS}}$`);
-const supportedChars = 'alphanumeric characters and underscores';
-export const usernameConstraints = `Username must be between ${MIN_USERNAME_CHARS} and ${MAX_USERNAME_CHARS} characters long and can contain only ${supportedChars}`;
-export const MAX_DISPLAY_NAME_CHARS = 50;
-export const MAX_BIO_CHARS = 160;
-
 export class UserProfileDto {
   @ApiProperty({
     description: "User's main wallet address"
@@ -22,73 +11,73 @@ export class UserProfileDto {
     message: 'Invalid address'
   })
   @Transform(normalizeAddressTransformer)
-  address!: string;
+  address: string;
 
   @ApiProperty({
     description: 'Non-unique name of the user'
   })
   @IsString()
   @MaxLength(MAX_DISPLAY_NAME_CHARS)
-  displayName!: string;
+  displayName: string;
 
   @ApiProperty({
     description: `Unique username for the user. ${usernameConstraints}`
   })
   @IsUsername({ message: `Invalid username. ${usernameConstraints}` })
-  username!: string;
+  username: string;
 
   @ApiProperty({
     description: `User's bio. Max ${MAX_BIO_CHARS} characters`
   })
   @IsString()
   @MaxLength(MAX_BIO_CHARS)
-  bio!: string;
+  bio: string;
 
   @ApiProperty({
     description: "User's profile image"
   })
   @IsString()
-  profileImage!: string;
+  profileImage: string;
 
   @ApiProperty({
     description: "User's banner image"
   })
   @IsString()
-  bannerImage!: string;
+  bannerImage: string;
 
   @ApiProperty({
     description: "User's discord username"
   })
   @IsString()
-  discordUsername!: string;
+  discordUsername: string;
 
   @ApiProperty({
     description: "User's twitter username"
   })
   @IsString()
-  twitterUsername!: string;
+  twitterUsername: string;
 
   @ApiProperty({
     description: "User's instagram username"
   })
   @IsString()
-  instagramUsername!: string;
+  instagramUsername: string;
 
   @ApiProperty({
     description: "User's facebook username"
   })
   @IsString()
-  facebookUsername!: string;
+  facebookUsername: string;
 
   @ApiProperty({
     description: "Date the user's profile was created. Epoch ms"
   })
   @IsNumber()
-  createdAt!: number;
+  createdAt: number;
 
   @ApiProperty({
     description: "Date the user's profile was updated. Epoch ms"
   })
   @IsNumber()
-  updatedAt!: number;
+  updatedAt: number;
 }
