f86faea4d677985e77e965e9b00e93538b696e9e
nneverlander <findadit@gmail.com>
2022-06-09 17:28:25 +0300

    removes logger

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,6 +5,5 @@ export * from './firestore';
 export * from './formatters';
 export * from './generic';
 export * from './links';
-export * from './logger';
 export * from './orders';
 export * from './user';
