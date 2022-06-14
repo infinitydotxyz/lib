8e803cb1b92e9508364ee859dbf2b0070194679a
nneverlander <findadit@gmail.com>
2022-06-14 11:57:01 +0300

    removes encode/decode docId

--- src/utils/firestore.ts
+++ src/utils/firestore.ts
@@ -7,21 +7,3 @@ export function getCollectionDocId(collection: { collectionAddress: string; chai
   }
   return `${collection.chainId}:${trimLowerCase(collection.collectionAddress)}`;
 }
-
-/**
- * Encodes a document id containing invalid characters.
- *
- * @see https://firebase.google.com/docs/firestore/quotas#collections_documents_and_fields
- */
-export function encodeDocId(value: string) {
-  return encodeURIComponent(value).replace(/\./g, '%2E');
-}
-
-/**
- * Decodes a document id containing invalid characters.
- *
- * @see https://firebase.google.com/docs/firestore/quotas#collections_documents_and_fields
- */
-export function decodeDocId(value: string) {
-  return decodeURIComponent(value.replace('%2E', '.'));
-}
