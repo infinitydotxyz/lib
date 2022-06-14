0e95bda7e95e876424cc1f3a8806192ca6228825
nneverlander <findadit@gmail.com>
2022-06-15 02:09:13 +0300

    zora types

--- src/types/services/zora/tokens.ts
+++ src/types/services/zora/tokens.ts
@@ -1,3 +1,49 @@
+import { Erc721Attribute } from '../../core';
+
+export interface ZoraTokensResponse {
+  tokens: {
+    nodes: ZoraToken[];
+    pageInfo: {
+      endCursor: string;
+      hasNextPage: boolean;
+      limit: number;
+    };
+  };
+}
+
+export interface ZoraToken {
+  token: {
+    tokenId: string;
+    name: string;
+    owner: string;
+    content: ZoraContent;
+    image: ZoraContent;
+    description: string;
+    tokenUrl: string;
+    tokenUrlMimeType: string;
+    attributes: Erc721Attribute[];
+    mintInfo: {
+      mintContext: {
+        blockNumber: number;
+        blockTimestamp: string; // rfc
+        transactionHash: string;
+      };
+      price: {
+        chainTokenPrice: {
+          currency: {
+            address: string;
+            decimals: number;
+            name: string;
+          };
+          decimal: number;
+        };
+      };
+      toAddress: string;
+      originatorAddress: string;
+    };
+  };
+}
+
 export interface ZoraContent {
   url: string;
   size: string;
