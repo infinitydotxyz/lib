0d9058c6c0a27f823e3cdb4f8554a37c0f29e11a
nneverlander <findadit@gmail.com>
2022-06-14 12:59:16 +0300

    adds attributeDocId function

--- src/utils/firestore.ts
+++ src/utils/firestore.ts
@@ -7,3 +7,12 @@ export function getCollectionDocId(collection: { collectionAddress: string; chai
   }
   return `${collection.chainId}:${trimLowerCase(collection.collectionAddress)}`;
 }
+
+export function getAttributeDocId(input: string) {
+  if (!input) {
+    throw new Error('Invalid attribute input');
+  }
+  // remove spaces, dashes, underscores, slashes
+  const output = input.replace(/[\s-_/\\]/g, '');
+  return output.toLowerCase();
+}
