2063d10d4c1dc72ced39be553e74af2735b675c5
Sleeyax <yourd3veloper@gmail.com>
2022-07-09 16:22:40 +0200

    minor: add timestamp & comments

--- src/types/dto/collections/curation/curated-collections.dto.ts
+++ src/types/dto/collections/curation/curated-collections.dto.ts
@@ -1,28 +1,54 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
-import { CuratedCollection } from '../../../core';
 import { CollectionDto } from '../collection.dto';
 
-export class CuratedCollectionDto implements CuratedCollection {
+export class CuratedCollectionDto {
+  /**
+   * Collection address.
+   */
   @ApiProperty()
   collectionAddress: string;
 
+  /**
+   * Collection chain id .
+   */
   @ApiProperty()
   collectionChainId: string;
 
+  /**
+   * Address of the user who curated this collection.
+   */
   @ApiProperty()
   userAddress: string;
 
+  /**
+   * User chain id.
+   */
   @ApiProperty()
   userChainId: string;
 
+  /**
+   * Amount of user votes.
+   */
   @ApiProperty()
   votes: number;
 
+  /**
+   * Fees accrued in USD.
+   */
   @ApiProperty()
   fees: number;
 
+  /**
+   * Fees APR as a percentage.
+   */
   @ApiProperty()
   feesAPR: number;
+
+  /**
+   * Timestamp indicating when this collection was curated by the user.
+   */
+  @ApiProperty()
+  timestamp: number;
 }
 
 export class CuratedCollectionsDataDto {
