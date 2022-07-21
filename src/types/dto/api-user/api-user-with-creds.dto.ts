9e27de3af2eb2a46bf9f8189c7fe6a56275ed9ea
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-07-21 16:00:28 -0500

    added api user public with creds dto

--- src/types/dto/api-user/api-user-with-creds.dto.ts
+++ src/types/dto/api-user/api-user-with-creds.dto.ts
@@ -2,9 +2,19 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { ValidateNested, IsNotEmpty } from 'class-validator';
 import { ApiUserCredsDto } from './api-user-creds.dto';
-import { ApiUserPublicDto } from './api-user.dto';
+import { ApiUserDto, ApiUserPublicDto } from './api-user.dto';
 
 export class ApiUserWithCredsDto extends ApiUserCredsDto {
+  @ApiProperty({
+    description: 'The user'
+  })
+  @ValidateNested()
+  @Type(() => ApiUserDto)
+  @IsNotEmpty()
+  user: ApiUserDto;
+}
+
+export class ApiUserPublicWithCredsDto extends ApiUserCredsDto {
   @ApiProperty({
     description: 'The user'
   })
