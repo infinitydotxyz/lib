d2babfa3fcc4ed7644c522d2d3aa9dc3eb0b775e
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-08-03 22:57:17 -0500

    updated nft stats to include more images

--- src/types/core/nft-stats.ts
+++ src/types/core/nft-stats.ts
@@ -1,25 +1,31 @@
 import { ChainId } from '../../../frontend/dist/types/core';
 import { NftSalesStats } from '../../../frontend/dist/types/core/Stats';
 import { NftImageDto } from '../dto/collections/nfts/nft-image.dto';
+import { ZoraContent } from '../services/zora';
 import { CollectionLinkData } from './CollectionStats';
 
 export interface NftLinkData {
   nftName: string;
 
-  nftTitle?: string;
+  nftTitle: string;
 
-  nftSlug?: string;
+  nftSlug: string;
 
-  rarityRank?: number;
+  rarityRank: number | null;
 
-  rarityScore?: number;
+  rarityScore: number | null;
 
-  nftImage: NftImageDto;
+  images: {
+    image: NftImageDto;
+    zoraImage: ZoraContent | null;
+    zoraContent: ZoraContent | null;
+    alchemyCachedImage: string;
+  };
 }
 
 export interface NftStats extends NftSalesStats, CollectionLinkData, NftLinkData {
   chainId: ChainId;
   collectionAddress: string;
   tokenId: string;
-  tokenIdNumber?: number;
+  tokenIdNumber: number | null;
 }
