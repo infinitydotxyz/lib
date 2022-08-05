1d541c090a991e784fee11e622aa6fc973626e4f
Sleeyax <yourd3veloper@gmail.com>
2022-08-05 12:58:54 +0200

    minor: add bulk vote DTO

--- src/types/dto/collections/curation/curation-vote.dto.ts
+++ src/types/dto/collections/curation/curation-vote.dto.ts
@@ -1,9 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNumber, Min } from 'class-validator';
+import { IsNumber, IsString, Min } from 'class-validator';
 
 export class CurationVoteDto {
   @IsNumber({ allowInfinity: false, allowNaN: false })
   @Min(1)
   @ApiProperty({ description: 'The number of votes to put into this collection', example: 10 })
   votes: number;
+}
+
+export class CurationBulkVoteDto extends CurationVoteDto {
+  @IsString()
+  @ApiProperty({ description: 'The collection to vote on' })
+  collection: string;
 }
