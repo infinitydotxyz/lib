614dbc57b2fe48037609fac60b612f113f1de766
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-08-10 13:57:17 -0500

    fixed lint error

--- src/types/dto/user/user-profile.dto.ts
+++ src/types/dto/user/user-profile.dto.ts
@@ -3,7 +3,6 @@ import { Transform } from 'class-transformer';
 import { IsEthereumAddress, IsNumber, IsString, MaxLength } from 'class-validator';
 import { IsUsername, MAX_BIO_CHARS, MAX_DISPLAY_NAME_CHARS, usernameConstraints } from '../../../decorators';
 import { normalizeAddressTransformer } from '../../../transformers/normalize-address.transformer';
-import { StakeInfo } from '../../core';
 export class UserProfileDto {
   @ApiProperty({
     description: "User's main wallet address"
@@ -93,10 +92,4 @@ export class UserProfileDto {
   })
   @IsNumber()
   totalCuratedVotes: number;
-}
-
-class UserCurationStakeDto {
-  stakeInfo: StakeInfo;
-  stakePower: number;
-  blockUpdatedAt: number;
 }
