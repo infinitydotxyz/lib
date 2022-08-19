097752ca6d20ee08ae6a74b4263462b433d64f42
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-08-18 19:10:08 -0500

    removed top curators from curation snippet

--- src/types/core/curation-ledger/curation-snippet.ts
+++ src/types/core/curation-ledger/curation-snippet.ts
@@ -1,6 +1,6 @@
 import { ChainId } from '../ChainId';
 import { CollectionDisplayData } from '../CollectionDisplayData';
-import { CurationBlockRewardsDoc, CurationBlockUser } from './curation-block';
+import { CurationBlockRewardsDoc } from './curation-block';
 import { CurationPeriodDoc } from './curation-period';
 
 export interface CurrentCurationSnippetMetadata {
@@ -19,10 +19,6 @@ export interface CurrentCurationSnippetDoc {
 
   currentBlock: Pick<CurationBlockRewardsDoc, 'metadata' | 'stats'> | null;
   mostRecentCompletedBlock: Pick<CurationBlockRewardsDoc, 'metadata' | 'stats'> | null;
-
-  topCuratorsByVotes: Pick<CurationBlockUser, 'user' | 'stats'>[];
-  topCuratorsByTotalProtocolFees: Pick<CurationBlockUser, 'user' | 'stats'>[];
-  earliestCurators: Pick<CurationBlockUser, 'user' | 'stats'>[];
 
   collection: CollectionDisplayData;
   metadata: CurrentCurationSnippetMetadata;
