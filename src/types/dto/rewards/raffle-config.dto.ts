06fcfc7b99cf5052d7ecf3437065d6bdaa51100d
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-09-29 09:58:21 -0500

    removed active phase ids from raffle config dto

--- src/types/dto/rewards/raffle-config.dto.ts
+++ src/types/dto/rewards/raffle-config.dto.ts
@@ -5,9 +5,5 @@ export class RaffleConfigDto {
    * percent of the total raffle rewards split for this phase that goes to this raffle
    */
   percentage: number;
-  /**
-   * ids of the phases that this raffle is active for
-   */
-  activePhaseIds: string[];
   ticketConfig: RaffleTicketConfigDto;
 }
