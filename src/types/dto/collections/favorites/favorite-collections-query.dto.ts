ac7bb94dae35ad7c91c7cac7280735584472aeb9
Sleeyax <yourd3veloper@gmail.com>
2022-10-07 16:20:19 +0200

    minor: add phase filter to query

--- src/types/dto/collections/favorites/favorite-collections-query.dto.ts
+++ src/types/dto/collections/favorites/favorite-collections-query.dto.ts
@@ -1,3 +1,20 @@
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import { IsEnum, IsOptional } from 'class-validator';
 import { PaginatedQuery } from '../../common';
 
-export class FavoriteCollectionsQueryDto extends PaginatedQuery {}
+export enum FavoriteCollectionPhaseFilter {
+  All = 'all',
+  Current = 'current',
+  Past = 'past'
+}
+
+export class FavoriteCollectionsQueryDto extends PaginatedQuery {
+  @ApiPropertyOptional({
+    description: 'Filter by phase',
+    enum: FavoriteCollectionPhaseFilter,
+    default: FavoriteCollectionPhaseFilter.All
+  })
+  @IsEnum(FavoriteCollectionPhaseFilter)
+  @IsOptional()
+  phase?: FavoriteCollectionPhaseFilter;
+}
