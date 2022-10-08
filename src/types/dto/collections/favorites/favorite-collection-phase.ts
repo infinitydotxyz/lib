0abaaa5060952aefcca04b52da53e7fb02eb325b
Sleeyax <yourd3veloper@gmail.com>
2022-10-08 16:37:24 +0200

    patch: include collectionPotFeesGenerated

--- src/types/dto/collections/favorites/favorite-collection-phase.ts
+++ src/types/dto/collections/favorites/favorite-collection-phase.ts
@@ -1,4 +1,10 @@
 import { PickType } from '@nestjs/swagger';
 import { TokenomicsPhaseDto } from '../../rewards';
 
-export class FavoriteCollectionPhaseDto extends PickType(TokenomicsPhaseDto, ['id', 'name', 'isActive', 'progress']) {}
+export class FavoriteCollectionPhaseDto extends PickType(TokenomicsPhaseDto, [
+  'id',
+  'name',
+  'isActive',
+  'progress',
+  'collectionPotFeesGenerated'
+]) {}
