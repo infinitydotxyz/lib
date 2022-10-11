161601bc2d873ab654bf4d18b9747ac4f8473c60
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-10-11 18:54:07 -0500

    updated user total rewards dto to include claiming data

--- src/types/dto/rewards/user-total-rewards.dto.ts
+++ src/types/dto/rewards/user-total-rewards.dto.ts
@@ -1,33 +1,27 @@
-import { ApiProperty } from '@nestjs/swagger';
+export class UserCumulativeRewardsDto {
+  contractAddress: string;
+  claimedWei: string;
+  claimedEth: number;
+  claimableWei: string;
+  claimableEth: number;
+  account: string;
+  cumulativeAmount: string;
+  merkleRoot: string;
+  merkleProof: string[];
+}
 
 export class UserTotalRewardsDto {
-  @ApiProperty({
-    description: 'Total volume the user has transacted. In eth'
-  })
-  userVolume: number;
+  tradingRefund: {
+    volume: number;
+    rewards: number;
+    sells: number;
+    buys: number;
+    claim: UserCumulativeRewardsDto;
+  };
 
-  @ApiProperty({
-    description: 'Total amount of veNFT rewards the user has gained'
-  })
-  userRewards: number;
-
-  @ApiProperty({
-    description: 'Total amount of sales the user has made'
-  })
-  userSells: number;
-
-  @ApiProperty({
-    description: 'Total number of purchases the user has made'
-  })
-  userBuys: number;
-
-  @ApiProperty({
-    description: 'Total amount of rewards gained via curation in wei'
-  })
-  userCurationRewardsWei: string;
-
-  @ApiProperty({
-    description: 'Total amount of rewards gained via curation in eth'
-  })
-  userCurationRewardsEth: number;
+  curation: {
+    totalRewardsWei: string;
+    totalRewardsEth: number;
+    claim: UserCumulativeRewardsDto;
+  };
 }
