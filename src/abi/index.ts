d271107780571e67de53fa45ce49f39d9b8742f1
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-10-12 08:39:55 -0500

    minor

--- src/abi/index.ts
+++ src/abi/index.ts
@@ -1,6 +1,7 @@
 export * from './erc1155';
 export * from './erc20';
 export * from './erc721';
+export * from './infinityCmDistributor';
 export * from './infinityExchange';
 export * from './infinityOBComplication';
 export * from './infinityStaker';
