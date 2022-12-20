5c8e6f1bbd6c2f37ba1d905f1f5db23ec3d67331
Joe Frazier <54604023+jfrazier-eth@users.noreply.github.com>
2022-12-20 16:20:49 -0600

    updated order status event type

--- src/types/core/orderbook/order-status-events.ts
+++ src/types/core/orderbook/order-status-events.ts
@@ -2,29 +2,12 @@ import { ChainId } from '../ChainId';
 import { ChainOBOrder } from '../OBOrder';
 import { OrderStatus } from './order-status';
 
-export enum OrderStatusEventKind {
-  Created = 'created',
-  Activated = 'activated',
-  Deactivated = 'deactivated'
-}
-
-export interface BaseOrderStatusEvent {
+export interface OrderStatusEvent {
   id: string;
   orderId: string;
   chainId: ChainId;
   status: OrderStatus;
   timestamp: number;
-  kind: OrderStatusEventKind;
-}
-
-export interface OrderStatusCreatedEvent extends BaseOrderStatusEvent {
   order: ChainOBOrder;
-  kind: OrderStatusEventKind.Created;
+  isMostRecent: boolean;
 }
-
-export interface OrderStatusChangedEvent extends BaseOrderStatusEvent {
-  kind: OrderStatusEventKind.Activated | OrderStatusEventKind.Deactivated;
-  prevStatus: OrderStatus;
-}
-
-export type OrderStatusEvent = OrderStatusCreatedEvent | OrderStatusChangedEvent;
