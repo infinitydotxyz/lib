b1acd6827e1d5f8dabba25e56f8d93c0911c526e
nneverlander <findadit@gmail.com>
2023-01-29 19:22:36 -0300

    adds coll historical sale type

---
+++ src/types/core/CollectionHistoricalSales.ts
@@ -0,0 +1,15 @@
+export interface CollectionHistoricalSale {
+  salePriceEth: number;
+  timestamp: number;
+  tokenImage: string;
+  tokenId: string;
+  collectionAddress: string;
+  collectionName: string;
+  chainId: string;
+  txHash: string;
+  marketplace: string;
+  marketplaceAddress: string;
+  seller: string;
+  buyer: string;
+  quantity: string;
+}
