c868e62ceaa44c0e7bdec9480c22fe729a79201b
nneverlander <findadit@gmail.com>
2023-01-29 19:28:23 -0300

    adds coll historical sale type

---
+++ src/types/dto/collections/collection-historical-sales-query.dto.ts
@@ -0,0 +1,35 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
+import { parseIntTransformer } from '../../../transformers/parse-int.transformer';
+
+export enum HistoricalSalesTimeBucket {
+  ONE_HOUR = '1h',
+  ONE_DAY = '1d',
+  ONE_WEEK = '7d',
+  ONE_MONTH = '30d',
+  ONE_YEAR = '1y'
+}
+
+export class CollectionHistoricalStatsQueryDto {
+  @ApiProperty({
+    description: 'Period to get stats for',
+    enum: HistoricalSalesTimeBucket
+  })
+  @IsEnum(HistoricalSalesTimeBucket)
+  period: HistoricalSalesTimeBucket;
+
+  @ApiProperty({
+    description: 'Number of data points to get'
+  })
+  @IsNumber()
+  @Transform(parseIntTransformer())
+  limit: number;
+
+  @ApiPropertyOptional({
+    description: 'Cursor to start after'
+  })
+  @IsString()
+  @IsOptional()
+  cursor?: string;
+}
