fe8a0f04893f0a591f8cb5e056d6f8cd9067a8b6
nneverlander <findadit@gmail.com>
2023-11-27 11:58:07 -0800

    adds more chain ids

--- src/types/core/ChainId.ts
+++ src/types/core/ChainId.ts
@@ -5,5 +5,12 @@
 export enum ChainId {
   Mainnet = '1',
   Goerli = '5',
-  Polygon = '137'
+  Polygon = '137',
+  Arbitrum = '42161',
+  Optimism = '10',
+  ArbitrumNova = '42170',
+  Base = '8453',
+  Zora = '7777777',
+  PolygonZKEVM = '1101',
+  Linea = '59144'
 }
